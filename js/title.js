df93d4817a19a6214c8eb39ac096cad74567052e
valsan-azerty-boi <52854501+valsan-azerty-boi@users.noreply.github.com>
2022-04-06 00:14:23 +0200

    update meta tag

--- js/title.js
+++ js/title.js
@@ -1,4 +1,4 @@
 let headTitle = "Guilde " + guildName + " (" + region.toUpperCase() + "-" + realm + ") World of Warcraft";
-$("meta[property='og\\title']").attr("content", headTitle);
+$("meta[property='og:title']").attr("content", headTitle);
 $("meta[name='title']").attr("content", headTitle);
 $(document).prop('title', headTitle);
