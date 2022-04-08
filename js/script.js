5777e984c8c98817c27323102c5cd413d90966b9
valsan-azerty-boi <52854501+valsan-azerty-boi@users.noreply.github.com>
2022-04-08 10:30:31 +0200

    add config.json, little refacto code

--- js/script.js
+++ js/script.js
@@ -9,145 +9,163 @@ const guildWarcraftLogsUri = "https://www.warcraftlogs.com/guild/{0}/{1}/{2}"; /
 const guildRaiderIoUri = "https://raider.io/guilds/{0}/{1}/{2}";
 
 const twitchStreamUri = "https://www.twitch.tv/{0}";
-const twitchStreamFrame = "https://player.twitch.tv/?channel={0}&enableExtensions=true&muted=true&parent=twitch.tv&player=popout&quality=chunked&volume=0";
+const twitchStreamFrame = "https://player.twitch.tv/?channel={0}&enableExtensions=false&muted=true&parent=twitch.tv&player=popout&quality=chunked&volume=0";
 
-let headTitle = "Guilde " + guildName + " (" + region.toUpperCase() + "-" + realm + ") World of Warcraft";
-$("meta[property='og:title']").attr("content", headTitle);
-$("meta[name='title']").attr("content", headTitle);
-$(document).prop('title', headTitle);
+fetch("./config.json")
+    .then(response => {
+        return response.json();
+    })
+    .then(dataFile => {
+        const guildName = dataFile.guildName;
+        const realm = dataFile.realm;
+        const region = dataFile.region;
+        const paragraphs = dataFile.paragraphs;
+        const raidTier = dataFile.raidTier;
+        const oldRaidTier = dataFile.oldRaidTier;
+        const youtubeLinks = dataFile.youtubeLinks;
+        const twitterLinks = dataFile.twitterLinks;
+        const facebookLinks = dataFile.facebookLinks;
+        const twitchStreamers = dataFile.twitchStreamers;
+        const discordId = dataFile.discordId;
 
-let bodyTitle = "Guilde &#60;" + guildName + "&#62; <br />" + region.toUpperCase() + " " + realm + " - World of Warcraft";
-$("#bodyTitle").append("<h1>" + bodyTitle + "<h1/>");
+        let headTitle = "Guilde " + guildName + " (" + region.toUpperCase() + "-" + realm + ") World of Warcraft";
+        $("meta[property='og:title']").attr("content", headTitle);
+        $("meta[name='title']").attr("content", headTitle);
+        $(document).prop('title', headTitle);
 
-if (!String.format) {
-    String.format = function (format) {
-        var args = Array.prototype.slice.call(arguments, 1);
-        return format.replace(/{(\d+)}/g, function (match, number) {
-            return typeof args[number] != 'undefined'
-                ? args[number]
-                : match
-                ;
-        });
-    };
-}
+        let bodyTitle = "Guilde &#60;" + guildName + "&#62; <br />" + region.toUpperCase() + " " + realm + " - World of Warcraft";
+        $("#bodyTitle").append("<h1>" + bodyTitle + "<h1/>");
 
-const kebabCase = (string) => {
-    return string.replace(/\d+/g, ' ')
-        .split(/ |\B(?=[A-Z])/)
-        .map((word) => word.toLowerCase())
-        .join('-');
-};
+        if (!String.format) {
+            String.format = function (format) {
+                var args = Array.prototype.slice.call(arguments, 1);
+                return format.replace(/{(\d+)}/g, function (match, number) {
+                    return typeof args[number] != 'undefined'
+                        ? args[number]
+                        : match
+                        ;
+                });
+            };
+        }
 
-function capitalize(sentence) {
-    return sentence && sentence[0].toUpperCase() + sentence.slice(1);
-}
+        const kebabCase = (string) => {
+            return string.replace(/\d+/g, ' ')
+                .split(/ |\B(?=[A-Z])/)
+                .map((word) => word.toLowerCase())
+                .join('-');
+        };
 
-$('#guildLinksWowArmory').attr('href', String.format(guildWowArmoryUri, region, kebabCase(realm), kebabCase(guildName)));
-$('#guildLinksWarcraftLogs').attr('href', String.format(guildWarcraftLogsUri, region, kebabCase(realm), guildName));
-$('#guildLinksRaiderIo').attr('href', String.format(guildRaiderIoUri, region, realm, guildName));
+        function capitalize(sentence) {
+            return sentence && sentence[0].toUpperCase() + sentence.slice(1);
+        }
 
-if (discordId !== null) {
-    $('#discordWidget').attr('src', String.format(discordWidgetUri, discordId));
-}
-else {
-    $("#discordWidgetDiv").remove();
-}
+        $('#guildLinksWowArmory').attr('href', String.format(guildWowArmoryUri, region, kebabCase(realm), kebabCase(guildName)));
+        $('#guildLinksWarcraftLogs').attr('href', String.format(guildWarcraftLogsUri, region, kebabCase(realm), guildName));
+        $('#guildLinksRaiderIo').attr('href', String.format(guildRaiderIoUri, region, realm, guildName));
 
-$('#oldProgressTitle').html("Anciens progress<br />");
-$(document).ready(function () {
-    $.ajax({
-        url: String.format(RaiderIoApiUri, region, realm, guildName),
-        type: "GET",
-        success: function (result) {
-            if (result.raid_progression[raidTier] == null) {
-                $('#progressWidgetMode').html("Progress non démarré<br />" + capitalize(raidTier.replaceAll('-', ' ')) + "<br />");
-                $("#progressGraphWidgetDiv").append("Rien à afficher pour l'instant");
-            }
-            else {
-                if (result.raid_progression[raidTier].mythic_bosses_killed > 0) {
-                    let progressWidgetPath = String.format(progressWidgetUri, raidTier, region, realm, guildName, "mythic");
-                    $('#progressWidgetMode').html("Progress " + result.raid_progression[raidTier].summary + "<br />" + capitalize(raidTier.replaceAll('-', ' ')) + "<br />");
-                    $('#progressWidget').attr('src', progressWidgetPath);
-                    let progressGraphWidgetPath = String.format(progressGraphWidgetUri, raidTier, "mythic", region, realm, guildName,);
-                    $('#progressGraphWidget').attr('src', progressGraphWidgetPath);
-                }
-                else if (result.raid_progression[raidTier].heroic_bosses_killed > 0) {
-                    let progressWidgetPath = String.format(progressWidgetUri, raidTier, region, realm, guildName, "heroic");
-                    $('#progressWidgetMode').html("Progress " + result.raid_progression[raidTier].summary + "<br />" + capitalize(raidTier.replaceAll('-', ' ')) + "<br />");
-                    $('#progressWidget').attr('src', progressWidgetPath);
-                    let progressGraphWidgetPath = String.format(progressGraphWidgetUri, raidTier, "heroic", region, realm, guildName,);
-                    $('#progressGraphWidget').attr('src', progressGraphWidgetPath);
-                } else {
-                    let progressWidgetPath = String.format(progressWidgetUri, raidTier, region, realm, guildName, "normal");
-                    $('#progressWidgetMode').html("Progress " + result.raid_progression[raidTier].summary + "<br />" + capitalize(raidTier.replaceAll('-', ' ')) + "<br />");
-                    $('#progressWidget').attr('src', progressWidgetPath);
-                    let progressGraphWidgetPath = String.format(progressGraphWidgetUri, raidTier, "normal", region, realm, guildName,);
-                    $('#progressGraphWidget').attr('src', progressGraphWidgetPath);
-                }
-            }
+        if (discordId !== null) {
+            $('#discordWidget').attr('src', String.format(discordWidgetUri, discordId));
+        }
+        else {
+            $("#discordWidgetDiv").remove();
+        }
 
-            oldRaidTier.forEach(t => {
-                if (result.raid_progression[t] == null) {
-                    $("#oldProgressContent").append("<br /><h2>" + capitalize(t.replaceAll('-', ' ')) + "<h2/><br />Rien à afficher pour ce raid");
-                }
-                else {
-                    var text = "<br /><h2>" + capitalize(t.replaceAll('-', ' ')) + " " + result.raid_progression[t].summary + "<h2/>";
-                    $("#oldProgressContent").append(text);
-
-                    if (result.raid_progression[t].mythic_bosses_killed > 0) {
-                        let progressWidgetPath = String.format(progressWidgetUri, t, region, realm, guildName, "mythic");
-                        $('#oldProgressContent').append("<iframe src=\"" + progressWidgetPath + "\" width=\"380\" height=\"135\" allowtransparency=\"true\" frameBorder=\"0\"></iframe>");
+        $('#oldProgressTitle').html("Anciens progress<br />");
+        $(document).ready(function () {
+            $.ajax({
+                url: String.format(RaiderIoApiUri, region, realm, guildName),
+                type: "GET",
+                success: function (result) {
+                    if (result.raid_progression[raidTier] == null) {
+                        $('#progressWidgetMode').html("Progress non démarré<br />" + capitalize(raidTier.replaceAll('-', ' ')) + "<br />");
+                        $("#progressGraphWidgetDiv").append("Rien à afficher pour l'instant");
                     }
-                    else if (result.raid_progression[t].heroic_bosses_killed > 0) {
-                        let progressWidgetPath = String.format(progressWidgetUri, t, region, realm, guildName, "heroic");
-                        $('#oldProgressContent').append("<iframe src=\"" + progressWidgetPath + "\" width=\"380\" height=\"135\" allowtransparency=\"true\" frameBorder=\"0\"></iframe>");
-                    } else {
-                        let progressWidgetPath = String.format(progressWidgetUri, t, region, realm, guildName, "normal");
-                        $('#oldProgressContent').append("<iframe src=\"" + progressWidgetPath + "\" width=\"380\" height=\"135\" allowtransparency=\"true\" frameBorder=\"0\"></iframe>");
+                    else {
+                        if (result.raid_progression[raidTier].mythic_bosses_killed > 0) {
+                            let progressWidgetPath = String.format(progressWidgetUri, raidTier, region, realm, guildName, "mythic");
+                            $('#progressWidgetMode').html("Progress " + result.raid_progression[raidTier].summary + "<br />" + capitalize(raidTier.replaceAll('-', ' ')) + "<br />");
+                            $('#progressWidget').attr('src', progressWidgetPath);
+                            let progressGraphWidgetPath = String.format(progressGraphWidgetUri, raidTier, "mythic", region, realm, guildName,);
+                            $('#progressGraphWidget').attr('src', progressGraphWidgetPath);
+                        }
+                        else if (result.raid_progression[raidTier].heroic_bosses_killed > 0) {
+                            let progressWidgetPath = String.format(progressWidgetUri, raidTier, region, realm, guildName, "heroic");
+                            $('#progressWidgetMode').html("Progress " + result.raid_progression[raidTier].summary + "<br />" + capitalize(raidTier.replaceAll('-', ' ')) + "<br />");
+                            $('#progressWidget').attr('src', progressWidgetPath);
+                            let progressGraphWidgetPath = String.format(progressGraphWidgetUri, raidTier, "heroic", region, realm, guildName,);
+                            $('#progressGraphWidget').attr('src', progressGraphWidgetPath);
+                        } else {
+                            let progressWidgetPath = String.format(progressWidgetUri, raidTier, region, realm, guildName, "normal");
+                            $('#progressWidgetMode').html("Progress " + result.raid_progression[raidTier].summary + "<br />" + capitalize(raidTier.replaceAll('-', ' ')) + "<br />");
+                            $('#progressWidget').attr('src', progressWidgetPath);
+                            let progressGraphWidgetPath = String.format(progressGraphWidgetUri, raidTier, "normal", region, realm, guildName,);
+                            $('#progressGraphWidget').attr('src', progressGraphWidgetPath);
+                        }
                     }
+
+                    oldRaidTier.forEach(t => {
+                        if (result.raid_progression[t] == null) {
+                            $("#oldProgressContent").append("<br /><h2>" + capitalize(t.replaceAll('-', ' ')) + "<h2/><br />Rien à afficher pour ce raid");
+                        }
+                        else {
+                            var text = "<br /><h2>" + capitalize(t.replaceAll('-', ' ')) + " " + result.raid_progression[t].summary + "<h2/>";
+                            $("#oldProgressContent").append(text);
+
+                            if (result.raid_progression[t].mythic_bosses_killed > 0) {
+                                let progressWidgetPath = String.format(progressWidgetUri, t, region, realm, guildName, "mythic");
+                                $('#oldProgressContent').append("<iframe src=\"" + progressWidgetPath + "\" width=\"380\" height=\"135\" allowtransparency=\"true\" frameBorder=\"0\"></iframe>");
+                            }
+                            else if (result.raid_progression[t].heroic_bosses_killed > 0) {
+                                let progressWidgetPath = String.format(progressWidgetUri, t, region, realm, guildName, "heroic");
+                                $('#oldProgressContent').append("<iframe src=\"" + progressWidgetPath + "\" width=\"380\" height=\"135\" allowtransparency=\"true\" frameBorder=\"0\"></iframe>");
+                            } else {
+                                let progressWidgetPath = String.format(progressWidgetUri, t, region, realm, guildName, "normal");
+                                $('#oldProgressContent').append("<iframe src=\"" + progressWidgetPath + "\" width=\"380\" height=\"135\" allowtransparency=\"true\" frameBorder=\"0\"></iframe>");
+                            }
+                        }
+                    });
+                },
+                error: function (error) {
+                    console.log(error);
+                    $("#progressWidgetDiv").append("<span style=\"color:red\"></span>");
+                    $("#progressGraphWidgetDiv").append("<span style=\"color:red\"></span>");
+                    $("#oldProgressDiv").append("<span style=\"color:red\"></span>");
                 }
             });
-        },
-        error: function (error) {
-            console.log(error);
-            $("#progressWidgetDiv").append("<span style=\"color:red\"></span>");
-            $("#progressGraphWidgetDiv").append("<span style=\"color:red\"></span>");
-            $("#oldProgressDiv").append("<span style=\"color:red\"></span>");
+        });
+
+        if (paragraphs !== null && paragraphs.length) {
+            paragraphs.forEach(p => {
+                $("#bodyParagraphs").append("<p>" + p + "<p/>");
+            });
+        }
+        else {
+            $("#bodyParagraphs").remove();
+        }
+
+        if (youtubeLinks !== null || twitterLinks !== null || facebookLinks !== null) {
+
+            if (youtubeLinks !== null)
+                $("#socialLinks").append("<a class=\"font-warcraft\" href=\"" + youtubeLinks + "\" target=\"_blank\"><img src=\"img/youtube-icon.png\" alt=\"Youtube\"/>&nbsp;Youtube</a>&nbsp;");
+
+            if (twitterLinks !== null)
+                $("#socialLinks").append("<a class=\"font-warcraft\" href=\"" + twitterLinks + "\" target=\"_blank\"><img src=\"img/twitter-icon.png\" alt=\"Twitter\"/>&nbsp;Twitter</a>&nbsp;");
+
+            if (facebookLinks !== null)
+                $("#socialLinks").append("<a class=\"font-warcraft\" href=\"" + facebookLinks + "\" target=\"_blank\"><img src=\"img/facebook-icon.png\" alt=\"Facebook\"/>&nbsp;Facebook</a>&nbsp;");
+        }
+
+        if (twitchStreamers !== null && twitchStreamers.length) {
+            twitchStreamers.forEach(streamer => {
+                $("#twitchStreamsLinks").append("<a class=\"font-warcraft\" href=\"" + String.format(twitchStreamUri, streamer) + "\" target=\"_blank\"><img src=\"img/twitch-icon.png\" alt=\"" + streamer + "\" />&nbsp;" + streamer + "<a/>&nbsp;");
+            });
+            $("#twitchStreamsLinks").append("<br />");
+            var rnd = Math.floor(Math.random() * twitchStreamers.length);
+            $("#twitchStreamsLinks").append("<br /><iframe class=\"responsive-iframe-stream\" width=\"450\" height=\"250\" allowtransparency=\"true\" frameborder=\"0\" src=\"" + String.format(twitchStreamFrame, twitchStreamers[rnd]) + "\"></iframe>");
+        }
+        else {
+            $("#twitchStreamsLinks").remove();
         }
     });
-});
-
-if (paragraphs !== null && paragraphs.length) {
-    paragraphs.forEach(p => {
-        $("#bodyParagraphs").append("<p>" + p + "<p/>");
-    });
-}
-else {
-    $("#bodyParagraphs").remove();
-}
-
-if (youtubeLinks !== null || twitterLinks !== null || facebookLinks !== null) {
-
-    if (youtubeLinks !== null)
-        $("#socialLinks").append("<a class=\"font-warcraft\" href=\"" + youtubeLinks + "\" target=\"_blank\"><img src=\"img/youtube-icon.png\" alt=\"Youtube\"/>&nbsp;Youtube</a>&nbsp;");
-
-    if (twitterLinks !== null)
-        $("#socialLinks").append("<a class=\"font-warcraft\" href=\"" + twitterLinks + "\" target=\"_blank\"><img src=\"img/twitter-icon.png\" alt=\"Twitter\"/>&nbsp;Twitter</a>&nbsp;");
-
-    if (facebookLinks !== null)
-        $("#socialLinks").append("<a class=\"font-warcraft\" href=\"" + facebookLinks + "\" target=\"_blank\"><img src=\"img/facebook-icon.png\" alt=\"Facebook\"/>&nbsp;Facebook</a>&nbsp;");
-}
-
-if (twitchStreamers !== null && twitchStreamers.length) {
-    twitchStreamers.forEach(streamer => {
-        $("#twitchStreamsLinks").append("<a class=\"font-warcraft\" href=\"" + String.format(twitchStreamUri, streamer) + "\" target=\"_blank\"><img src=\"img/twitch-icon.png\" alt=\"" + streamer + "\" />&nbsp;" + streamer + "<a/>&nbsp;");
-    });
-    $("#twitchStreamsLinks").append("<br />");
-    var rnd = Math.floor(Math.random() * twitchStreamers.length);
-    $("#twitchStreamsLinks").append("<br /><iframe class=\"responsive-iframe-stream\" width=\"450\" height=\"250\" allowtransparency=\"true\" frameborder=\"0\" src=\"" + String.format(twitchStreamFrame, twitchStreamers[rnd]) + "\"></iframe>");
-}
-else {
-    $("#twitchStreamsLinks").remove();
-}
 
 document.getElementById("year").innerHTML = new Date().getFullYear();
